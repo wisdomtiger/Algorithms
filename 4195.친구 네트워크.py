45e6071e629db61809a4c1f9889cb953171b862a
wisdomtiger <69517365+wisdomtiger@users.noreply.github.com>
2020-08-21 18:50:24 +0900

    Update 4195.친구 네트워크.py

--- 4195.친구 네트워크.py
+++ 4195.친구 네트워크.py
@@ -1,9 +1,36 @@
-n = int(input())
+def find(x):
+    if x == parent[x]:
+        return x
+    else:
+        p = find(parent[x])
+        parent[x] = p
+        return parent[x]
 
-for i in range(n):
-    friends = set([])
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x != y:y
+        parent[y] = x
+        number[x] += number[y]
+
+test_case = int(input())
+
+for _ in range(test_case):
+    parent = dict()
+    number = dict()
+
     f = int(input())
-    for j in range(f):
-        member = list(input().split())
-        friends.update(member)
-        print(len(friends))
+
+    for _ in range(f):
+        x,y = input().split(' ')
+
+        if x not in parent:
+            parent[x] = x
+            number[x] = 1
+        if y not in parent:
+            parent[y] = y
+            number[y] = 1
+
+        union(x, y)
+
+        print(number[find(x)])
