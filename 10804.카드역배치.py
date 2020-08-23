a7cde1cc0ee9697d8a125517946b8a51074fd085
wisdomtiger <69517365+wisdomtiger@users.noreply.github.com>
2020-08-23 10:31:06 +0900

    Update 10804.카드역배치.py

--- 10804.카드역배치.py
+++ 10804.카드역배치.py
@@ -3,12 +3,15 @@ start_po, end_po = 0, 0
 
 for i in range(10):
     start_po, end_po = map(int, input().split())
+    
     if(start_po == end_po):
         continue
+        
     start_po -= 1
     end_po -= 1
 
     re = int((end_po-start_po+1)/2)
+    
     for k in range(re):
         arr[start_po], arr[end_po] = arr[end_po], arr[start_po]
         start_po += 1
