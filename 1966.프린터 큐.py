104dd38802db0adc2e1ec2f21ebe622c7230c4ab
wisdomtiger <69517365+wisdomtiger@users.noreply.github.com>
2020-08-24 16:08:05 +0900

    Update 1966.프린터 큐.py

--- 1966.프린터 큐.py
+++ 1966.프린터 큐.py
@@ -4,8 +4,10 @@ result = []
 for i in range(test):
     count = 0
     fir_in = 0
+    
     n, m = map(int, input().split(' '))
     que = list(map(int, input().split(' ')))
+    
     while True:
         if(que[fir_in]==max(que)):
             if(fir_in==m):
