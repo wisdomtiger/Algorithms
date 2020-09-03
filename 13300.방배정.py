dd23ccb3cd83eda523a61b16dc3c24a17cd649cc
wisdomtiger <69517365+wisdomtiger@users.noreply.github.com>
2020-09-03 23:27:49 +0900

    Update 13300.방배정.py

--- 13300.방배정.py
+++ 13300.방배정.py
@@ -8,6 +8,7 @@ room = 0
 for i in range(n):
     s, y = map(int, sys.stdin.readline().split())
     matrix[s][y-1] += 1
+    
 for i in range(2):
     for j in range(6):
         if(matrix[i][j]==0):
